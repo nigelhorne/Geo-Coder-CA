6afd1e45cdea75652338c14502d5193fb7508007
Nigel Horne <njh@bandsman.co.uk>
2017-10-28 10:07:48 -0400

    Release 0.06

--- lib/Geo/Coder/CA.pm
+++ lib/Geo/Coder/CA.pm
@@ -19,11 +19,11 @@ Geo::Coder::CA - Provides a geocoding functionality using http:://geocoder.ca fo
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
